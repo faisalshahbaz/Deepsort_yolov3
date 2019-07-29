68007de0ecca2c90df76741285a5994d8425ef90
Li Jinjie <lijinjie362@outlook.com>
2019-07-29 10:56:41 +0800

    update

--- yolov3_tf.py
+++ yolov3_tf.py
@@ -1,3 +1,7 @@
+"""
+Run a tensorflow YOLO_v3 style detection model.
+"""
+
 import cv2
 import numpy as np
 import core.utils as utils
@@ -15,33 +19,49 @@ input_size = 416
 graph = tf.Graph()
 
 
-def detect_image(original_image):
-    # original_image = cv2.imread(image_path)
-    # original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-    original_image_size = original_image.shape[:2]
-    image_data = utils.image_preporcess(np.copy(original_image),
-                                        [input_size, input_size])
-    image_data = image_data[np.newaxis, ...]
+class YOLOV3(object):  # Object类是所有类都会继承的类
+    def __init__(self):
+        self.return_elements = [
+            "input/input_data:0", "pred_sbbox/concat_2:0",
+            "pred_mbbox/concat_2:0", "pred_lbbox/concat_2:0"
+        ]
+        self.pb_file = "./yolov3_coco.pb"
+        self.num_classes = 80
+        self.input_size = 416
+        self.graph = tf.Graph()
+        self.return_tensors = utils.read_pb_return_tensors(
+            graph, pb_file, return_elements)
+        self.sess = tf.Session(graph=graph)
 
-    return_tensors = utils.read_pb_return_tensors(graph, pb_file,
-                                                  return_elements)
+    def close_session(self):
+        self.sess.close()
 
-    with tf.Session(graph=graph) as sess:
-        pred_sbbox, pred_mbbox, pred_lbbox = sess.run(
-            [return_tensors[1], return_tensors[2], return_tensors[3]],
-            feed_dict={return_tensors[0]: image_data})
+    def detect_image(self, original_image):
 
-    pred_bbox = np.concatenate([
-        np.reshape(pred_sbbox, (-1, 5 + num_classes)),
-        np.reshape(pred_mbbox, (-1, 5 + num_classes)),
-        np.reshape(pred_lbbox, (-1, 5 + num_classes))
-    ],
-                               axis=0)
+        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+        original_image_size = original_image.shape[:2]
+        image_data = utils.image_preporcess(np.copy(original_image),
+                                            [input_size, input_size])
+        image_data = image_data[np.newaxis, ...]
 
-    bboxes = utils.postprocess_boxes(pred_bbox, original_image_size,
-                                     input_size, 0.3)
-    return bboxes
-    # bboxes = utils.nms(bboxes, 0.45, method='nms')
-    # image = utils.draw_bbox(original_image, bboxes)
-    # image = Image.fromarray(image)
-    # image.show()
+        pred_sbbox, pred_mbbox, pred_lbbox = self.sess.run(
+            [
+                self.return_tensors[1], self.return_tensors[2],
+                self.return_tensors[3]
+            ],
+            feed_dict={self.return_tensors[0]: image_data})
+
+        pred_bbox = np.concatenate([
+            np.reshape(pred_sbbox, (-1, 5 + num_classes)),
+            np.reshape(pred_mbbox, (-1, 5 + num_classes)),
+            np.reshape(pred_lbbox, (-1, 5 + num_classes))
+        ],
+                                   axis=0)
+
+        bboxes = utils.postprocess_boxes(pred_bbox, original_image_size,
+                                         input_size, 0.3)
+        return bboxes
+        # bboxes = utils.nms(bboxes, 0.45, method='nms')
+        # image = utils.draw_bbox(original_image, bboxes)
+        # image = Image.fromarray(image)
+        # image.show()
